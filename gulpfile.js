031a1e5c0d63b74d2fefe038d6cb73ab8f9bf367
jgarcia77 <jgarcia773@gmail.com>
2017-12-07 18:03:18 -0600

    React: Creating Components Introduction

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ var config = {
     paths: {
         html: './src/*.html',
         js: './src/**/*.js',
+        images: './src/images/*',
         css: [
             'node_modules/bootstrap/dist/css/bootstrap.min.css',
             'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
@@ -68,10 +69,16 @@ gulp.task('js', function() {
         .pipe(connect.reload());
 });
 
+gulp.task('images', function() {
+    gulp.src(config.paths.images)
+        .pipe(gulp.dest(config.paths.dist + '/images'))
+        .pipe(connect.reload());
+});
+
 gulp.task('lint', function() {
     return gulp.src(config.paths.js)
             .pipe(lint({config: 'eslint.config.json'}))
             .pipe(lint.format());
 });
 
-gulp.task('default', ['html', 'js', 'css', 'lint', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'css', 'images', 'lint', 'open', 'watch']);
