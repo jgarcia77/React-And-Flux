5b29dd816b7e3671f7989d2f938ab5f1bfa6054a
jgarcia77 <jgarcia773@gmail.com>
2017-12-07 18:54:01 -0600

    React Lifecycle

---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            'id': 'corey-house',
+            'firstName': 'Corey',
+            'lastName': 'House'
+        },
+        {
+            'id': 'scott-allen',
+            'firstName': 'Scott',
+            'lastName': 'Allen'
+        },
+        {
+            'id': 'dan-wahlin',
+            'firstName': 'Dan',
+            'lastName': 'Wahlin'
+        }
+    ]
+};
